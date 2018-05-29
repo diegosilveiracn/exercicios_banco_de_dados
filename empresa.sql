85e8303b4a201ba1a6ee7adc5f77abae47938733
Diego <diego@MacBook-Air-de-Diego.local>
2018-05-28 22:43:45 -0300

    Melhorando o script do banco.

--- empresa.sql
+++ empresa.sql
@@ -2,7 +2,7 @@
 create database empresa;
 
 -- Tabelas
-create table empregado(
+create table empresa.empregado(
 pnome varchar(20),
 minicial char,
 unome varchar(20),
@@ -15,32 +15,32 @@ superssn integer,
 dno integer
 );
 
-create table departamento(
+create table empresa.departamento(
 dnome varchar(30),
 dnumero integer,
 gerssn integer,
 gerdatainicio date
 );
 
-create table dept_localizacoes(
+create table empresa.dept_localizacoes(
 dnumero integer,
 dlocalizacao varchar(30)
 );
 
-create table trabalha_em(
+create table empresa.trabalha_em(
 essn integer,
 pno integer,
 horas decimal(3,1)
 );
 
-create table projeto(
+create table empresa.projeto(
 pjnome varchar(30),
 pnumero integer,
 plocalizacao varchar(30),
 dnum integer
 );
 
-create table dependente(
+create table empresa.dependente(
 essn integer,
 nome_dependente varchar(50),
 sexo char,
@@ -49,30 +49,33 @@ parentesco varchar(20)
 );
 
 -- Povoando
-insert into empregado (pnome, minicial, unome, ssn, datanasc,endereco, sexo, salario, superssn, dno)
+insert into empresa.empregado (pnome,minicial,unome,ssn,datanasc,endereco,sexo,salario,superssn,dno)
 values
-('John','B','Smith',123456789,'1965-01-09','731 Fondren, Houston, TX', 'M', 30000, 333445555, 5),
-('Franklin','T','Wong',333445555,'1955-12-08','683 Voss, Houston, TX', 'M', 40000, 888665555,5),
-('Alicia','J','Zelaya',999887777,'1968-01-19','3321 Castle, Spring, TX', 'F', 25000, 987654321,4),
-('Jennifer','S','Wallace',987654321,'1941-06-20','291 Berry, Bellaire, TX', 'F', 43000, 888665555,4),
-('Ramesh','K','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble, TX', 'M', 38000,
+('John','B','Smith',123456789,'1965-01-09','731 Fondren, Houston, TX','M',30000,333445555,5),
+('Franklin','T','Wong',333445555,'1955-12-08','683 Voss, Houston, TX','M',40000,888665555,5),
+('Alicia','J','Zelaya',999887777,'1968-01-19','3321 Castle, Spring, TX','F',25000,987654321,4),
+('Jennifer','S','Wallace',987654321,'1941-06-20','291 Berry, Bellaire, TX','F',43000,888665555,4),
+('Ramesh','K','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble, TX','M',38000,
 333445555,5),
-('Joyce','A','English',453453453,'1972-07-31','5631 Rice, Houston, TX', 'F', 25000, 333445555,5),
-('Ahmad','V','Jabbar',987987987,'1969-03-29','980 Dallas, Houston, TX', 'M', 25000, 987654321,4),
-('James','E','Borg',888665555,'1937-11-10','450 Stone, Houston, TX', 'M', 55000, null,1);
-insert into departamento (dnome, dnumero, gerssn, gerdatainicio)
+('Joyce','A','English',453453453,'1972-07-31','5631 Rice, Houston, TX', 'F',25000,333445555,5),
+('Ahmad','V','Jabbar',987987987,'1969-03-29','980 Dallas, Houston, TX', 'M',25000,987654321,4),
+('James','E','Borg',888665555,'1937-11-10','450 Stone, Houston, TX', 'M',55000,null,1);
+
+insert into empresa.departamento (dnome,dnumero,gerssn,gerdatainicio)
 values
 ('Pesquisa',5,333445555,'1968-05-22'),
 ('Administração',4,987654321,'1995-01-01'),
 ('Sede administrativa',1,888665555,'1981-06-19');
-insert into dept_localizacoes (dnumero, dlocalizacao)
+
+insert into empresa.dept_localizacoes (dnumero,dlocalizacao)
 values
 (1,'Houstong'),
 (4,'Stafford'),
 (5,'Bellaire'),
 (5,'Sugarland'),
 (4,'Houston');
-insert into trabalha_em (essn, pno, horas)
+
+insert into empresa.trabalha_em (essn,pno,horas)
 values
 (123456789, 1, 35.5),
 (123456789,2,7.5),
@@ -90,7 +93,8 @@ values
 (987654321,30,20.0),
 (987654321,20,15.0),
 (888665555,20,null);
-insert into projeto (pjnome, pnumero, plocalizacao, dnum)
+
+insert into empresa.projeto (pjnome,pnumero,plocalizacao,dnum)
 values
 ('ProdutoX', 1, 'Bellaire',5),
 ('ProdutoY', 2, 'Sugarland',5),
@@ -98,7 +102,8 @@ values
 ('Automatização', 10, 'Stafford',4),
 ('Reorganização', 20, 'Houston',1),
 ('Novos Benefícios', 30, 'Stafford',4);
-insert into dependente (essn, nome_dependente, sexo, datanasc, parentesco)
+
+insert into empresa.dependente (essn,nome_dependente,sexo,datanasc,parentesco)
 values
 (333445555, 'Alice', 'F','1986-04-05','FILHA'),
 (333445555, 'Theodore', 'M','1983-10-25','FILHO'),
@@ -109,18 +114,18 @@ values
 (123456789, 'Elizabeth', 'F','1967-05-05','CÔNJUGE');
 
 -- Restrições de integridades: chave e referencial
-alter table empregado add constraint pk_empregado primary key (ssn);
-alter table departamento add constraint pk_departamento primary key (dnumero);
-alter table dept_localizacoes add constraint pk_dept_localizacoes primary key (dnumero, dlocalizacao);
-alter table trabalha_em add constraint pk_trabalha_em primary key (essn, pno);
-alter table projeto add constraint pk_projeto primary key (pnumero);
-alter table dependente add constraint pk_dependente primary key (essn, nome_dependente);
-alter table empregado add constraint fk_empregado_empregado foreign key (superssn) references empregado(ssn);
-alter table empregado add constraint fk_empregado_departamento foreign key (dno) references departamento(dnumero);
-alter table departamento add constraint fk_departamento_empregado foreign key (gerssn) references empregado(ssn);
-alter table dept_localizacoes add constraint fk_dept_localizacoes_departamento foreign key (dnumero) references
-departamento(dnumero);
-alter table trabalha_em add constraint fk_trabalha_em_empregado foreign key (essn) references empregado(ssn);
-alter table trabalha_em add constraint fk_trabalha_em_projeto foreign key (pno) references projeto(pnumero);
-alter table projeto add constraint fk_projeto_departamento foreign key (dnum) references departamento(dnumero);
-alter table dependente add constraint fk_dependente_empregado foreign key (essn) references empregado(ssn);
+alter table empresa.empregado add constraint pk_empregado primary key (ssn);
+alter table empresa.departamento add constraint pk_departamento primary key (dnumero);
+alter table empresa.dept_localizacoes add constraint pk_dept_localizacoes primary key (dnumero, dlocalizacao);
+alter table empresa.trabalha_em add constraint pk_trabalha_em primary key (essn, pno);
+alter table empresa.projeto add constraint pk_projeto primary key (pnumero);
+alter table empresa.dependente add constraint pk_dependente primary key (essn, nome_dependente);
+alter table empresa.empregado add constraint fk_empregado_empregado foreign key (superssn) references empresa.empregado(ssn);
+alter table empresa.empregado add constraint fk_empregado_departamento foreign key (dno) references empresa.departamento(dnumero);
+alter table empresa.departamento add constraint fk_departamento_empregado foreign key (gerssn) references empresa.empregado(ssn);
+alter table empresa.dept_localizacoes add constraint fk_dept_localizacoes_departamento foreign key (dnumero) references
+empresa.departamento(dnumero);
+alter table empresa.trabalha_em add constraint fk_trabalha_em_empregado foreign key (essn) references empresa.empregado(ssn);
+alter table empresa.trabalha_em add constraint fk_trabalha_em_projeto foreign key (pno) references empresa.projeto(pnumero);
+alter table empresa.projeto add constraint fk_projeto_departamento foreign key (dnum) references empresa.departamento(dnumero);
+alter table empresa.dependente add constraint fk_dependente_empregado foreign key (essn) references empresa.empregado(ssn);
